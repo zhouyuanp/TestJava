eaf079cd41c6c08c794eafa168472952674f2b0e
yuanpengzhou <zyp112490@163.com>
2019-07-31 08:59:42 +0800

    接口和类的关系

--- src/MyPro06/dome02/MyInterface.java
+++ src/MyPro06/dome02/MyInterface.java
@@ -1,11 +1,22 @@
 package src.MyPro06.dome02;
 
 public interface MyInterface {
+
+    public static final String name2 = "张三";
+    String name = "张三";
     void show();
     String fun();
     int [] method();
-   /*
+   /**
     public void MyInterface(){
 
     }*/
+   /**
+    * jdk 1.8 的新特性
+    * 非抽象方法必须使用default 来修饰
+    *
+    */
+   public default void method2(){
+
+   }
 }
---
+++ src/MyPro06/dome02/Play.java
@@ -0,0 +1,6 @@
+package src.MyPro06.dome02;
+
+public interface Play {
+
+    public void show(String songName);
+}
