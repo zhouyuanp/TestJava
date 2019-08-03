f707e8bce5a57ef7c6a19ae6751c053abe0a862f
yuanpengzhou <zyp112490@163.com>
2019-08-03 10:06:42 +0800

    内部类成员内部类，静态内部类，方法中的内部类，匿名内部类

---
+++ src/MyPro06/dome04/MethodOuter.java
@@ -0,0 +1,31 @@
+package src.MyPro06.dome04;
+
+/**
+ * @program: TestJava
+ * @description:
+ * @author: zhouyuanpeng
+ * Created by  2019-08-03 09:23
+ */
+
+public class MethodOuter {
+
+    //show 是 MethodOuter 成员方法
+    public void show(){
+
+        int a = 10; //方法中的局部变量
+        // Inner 内部类相当于show 方法中的局部变量，使用范围只能在show 方法中
+        class Inner{   //show 方法中的内部类 只能在方法中使用，出了方法没有意义
+            public void fun(){
+                System.out.println("a="+a);
+
+            }
+
+        }
+
+        //创建内部类的对象
+
+        new Inner().fun();
+    }
+
+
+}
---
+++ src/MyPro06/dome04/TestMethodOuter.java
@@ -0,0 +1,20 @@
+package src.MyPro06.dome04;
+
+/**
+ * @program: TestJava
+ * @description: 测试方法中的内部类的访问
+ * @author: zhouyuanpeng
+ * Created by  2019-08-03 09:30
+ */
+
+public class TestMethodOuter {
+    public static void main(String[] args) {
+
+
+
+    }
+
+
+
+
+}
