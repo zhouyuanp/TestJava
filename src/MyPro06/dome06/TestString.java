387a8148a4d3f5ba7ee1d00502ac0da83f512549
yuanpengzhou <zyp112490@163.com>
2019-08-06 19:13:59 +0800

    String 常量池和构造方法

--- src/MyPro06/dome06/TestString.java
+++ src/MyPro06/dome06/TestString.java
@@ -1,5 +1,12 @@
 package src.MyPro06.dome06;
 
+/**
+ * @program: TestJava
+ * @description:  测试 String 类的构造方法
+ * @author: zhouyuanpeng
+ * Created by  2019-08-06 16:35
+ */
+
 public class TestString {
     public static void main(String[] args){
         String str = "apple";
@@ -19,7 +26,32 @@ public class TestString {
          * String 类的构造方法
          * */
         String s1 = new String(); //创建了一个长度为0的字符串
-        String s2 = null;
+        String s2 = null; // 没有创建对象，只是声明了String类型的对象s2
+        String s3 = new String("hello");
+
+
+        //通过char类型的数组构造String对象
+        char [] c = {'a','b','c'};
+        String s4 = new String(c);
+
+        //通过byte类型的数组构造String对象
+
+        byte [] b = {97,98,99};
+        String s5 = new String(b);
+
+        String s6 = new String(c,0,2);
+        System.out.println("s6:"+s6);
+
+        String s7 = new String(b,1,2);
+        System.out.println("s7:"+s7);
+
+
+
+
+
+
+
+
 
 
 
