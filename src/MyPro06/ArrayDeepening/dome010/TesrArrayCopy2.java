d6b4f02e8a49eb13a5dc364af82a86a7fb8480bc
zhouyuanp <zyp112490@163.com>
2019-09-09 22:58:37 +0800

    拷贝元素

--- src/MyPro06/ArrayDeepening/dome010/TesrArrayCopy2.java
+++ src/MyPro06/ArrayDeepening/dome010/TesrArrayCopy2.java
@@ -4,7 +4,6 @@ public class TesrArrayCopy2 {
     public static void main(String[] args) {
         int[] arrA = {11,22,33,44};
         int[] arrB = new int[5];
-
         //复制
         /**
         arrB [0] = arrA[0];
@@ -13,20 +12,19 @@ public class TesrArrayCopy2 {
         arrB [3] = arrB[3];
         arrB [4] = arrA[4];
         */
-        int length = Math.min(arrA.length,arrB.length);
         System.out.println("赋值之前");
-        for (int i = 0;i< length;i++ ){
+        for (int i = 0;i< arrB.length;i++ ){
             System.out.print(arrB[i]+"\t");
         }
-
+        //两个数组的长度的最小值
+        int length = Math.min(arrA.length,arrB.length);
         for (int i = 0;i<length;i++){
             arrB[i] = arrA[i];
         }
         //遍历arrB数组
         System.out.println("\n赋值之后");
-        for (int i = 0;i< length;i++ ){
+        for (int i = 0;i<arrB.length;i++ ){
             System.out.print(arrB[i]+"\t");
         }
-
     }
 }
