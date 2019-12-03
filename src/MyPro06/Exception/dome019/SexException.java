3c48eaaee5777da5139a91cc76767d3ffe516579
yuanpengzhou <zyp112490@163.com>
2019-12-03 13:05:49 +0800

    自定义异常

---
+++ src/MyPro06/Exception/dome019/SexException.java
@@ -0,0 +1,26 @@
+package src.MyPro06.Exception.dome019;
+
+/**
+ * @program: TestJava
+ * @description:
+ * 定义异常类
+ * 1.继承Exception 类
+ * 2.编写构造方法
+ * @author: zhouyuanpeng
+ * Created by  2019-12-03 12:01
+ *
+ */
+
+public class SexException extends Exception {
+
+    public SexException(){
+            super();
+
+        }
+    public SexException(String message){
+        super(message);
+
+    }
+
+
+}
---
+++ src/MyPro06/Exception/dome019/TestSexException.java
@@ -0,0 +1,33 @@
+package src.MyPro06.Exception.dome019;
+
+import java.util.Scanner;
+
+/**
+ * @program: TestJava
+ * @description: 自定义异常
+ * @author: zhouyuanpeng
+ * Created by  2019-12-03 12:50
+ */
+
+public class TestSexException {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("请输入您的性别：");
+        String gender = input.next();
+
+        if ("男".equals(gender)||"女".equals(gender)){
+            System.out.println("性别录入正确。");
+        }else{
+            try {
+                throw new SexException("性别只能是男或者女");
+            } catch (SexException e) {
+                e.printStackTrace();
+                System.out.println(e.getMessage());
+            }
+        }
+
+
+    }
+}
