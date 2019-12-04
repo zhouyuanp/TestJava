22ddbc9d10253365f3d05655c037a776e4791bf2
yuanpengzhou <zyp112490@163.com>
2019-12-04 14:49:54 +0800

    测试自动装箱和拆箱

---
+++ src/MyPro06/CommonClasses/dome21/test.java
@@ -0,0 +1,14 @@
+package src.MyPro06.CommonClasses.dome21;
+
+/**
+ * @program: TestJava
+ * @description: 测试自动装箱和拆箱
+ * @author: zhouyuanpeng
+ * Created by  2019-12-04 14:48
+ */
+
+public class test {
+    public static void main(String[] args) {
+
+    }
+}
