ad11ba6ec7babb75c7cacc033c7b3a0f0640daa1
yuanpengzhou <zyp112490@163.com>
2019-12-04 14:48:12 +0800

    测试包装常用类

---
+++ src/MyPro06/CommonClasses/dome20/test.java
@@ -0,0 +1,15 @@
+package src.MyPro06.CommonClasses.dome20;
+
+/**
+ * @program: TestJava
+ * @description: 测试常用包装类
+ * @author: zhouyuanpeng
+ * Created by  2019-12-04 14:47
+ */
+
+public class test {
+    public static void main(String[] args) {
+
+    }
+
+}
